9976a2627b6f14cbf26f525f26c0fa5d8e6c4bc8
Stephanie <srimel1@student.gsu.edu>
2022-09-02 00:44:05 -0400

    g and h

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace CompassInterviewTest
     public partial class MainWindow : Window
     {
         private MainWindowViewModel _viewModel;
-        
+
         public MainWindow()
         {
             _viewModel = new MainWindowViewModel();
@@ -35,66 +35,83 @@ namespace CompassInterviewTest
             InitializeComponent();
         }
 
-        private void UpdateCalculatedIntensity()
+        public void UpdateCalculatedIntensity()
         {
             //Todo B. Modify this variable assignment to calculate the intensity of the light using OpacityDistanceFunction based on the chosen values in the viewModel and set it to calculatedIntensityLabel.Content
             //Todo (Make sure to convert the ChosenFrequency value to exahertz by multiplying it by 1.0E18)
-            calculatedIntensityLabel.Content = _viewModel.ChosenIntensity;
+            // calculatedIntensityLabel.Content = _viewModel.ChosenIntensity;
+            //i dont know the distance from the light source
+            calculatedIntensityLabel.Content = OpacityCalculatorConstants.OpacityDistanceFunction(
+                _viewModel.ChosenMaterialType, _viewModel.ChosenFrequency * 1.0E18, _viewModel.ChosenDistance,
+                _viewModel.ChosenDistance);
+
+            // var valid = ValidateInputVariables(out var errorMessage);
+            // if (valid == false)
+            // {
+            //     calculatedIntensityLabel.Content = "N/A";
+            // }
+            // else
+            // {
+            //     calculatedIntensityLabel.Content =OpacityCalculatorConstants.OpacityDistanceFunction(_viewModel.ChosenMaterialType, _viewModel.ChosenFrequency*1.0E18, _viewModel.ChosenDistance, _viewModel.ChosenDistance-OpacityCalculatorConstants.MinimumDistance);
+            //     
+            // }
         }
 
-        //Todo F. Modify this function to set an error message to errorMessage and return false if any of the input variables are outside of the ranges specified in OpacityCalculatorConstants
-        //Todo (Make sure to convert the ChosenFrequency value to exahertz by multiplying it by 1.0E18)
+        // private void UpdateCalculatedIntensity(string contentLabel)
+        // {
+        //     calculatedIntensityLabel.Content = contentLabel;
+        // }
+
+        //Modify this function to set an error message to errorMessage and return false if any of the input variables are outside of the ranges specified in OpacityCalculatorConstants
+        // (Make sure to convert the ChosenFrequency value to exahertz by multiplying it by 1.0E18)
         private bool ValidateInputVariables(out string errorMessage)
         {
             errorMessage = "";
 
-            if (_viewModel.ChosenFrequency * 1.0E18 < OpacityCalculatorConstants.MinimumFrequency  ||
+            if (_viewModel.ChosenFrequency * 1.0E18 < OpacityCalculatorConstants.MinimumFrequency ||
                 _viewModel.ChosenFrequency * 1.0E18 > OpacityCalculatorConstants.MaximumFrequency)
             {
-
-                errorMessage = "Frequency: "+_viewModel.ChosenFrequency+" Outside Range ["+OpacityCalculatorConstants.MinimumFrequency+", "+OpacityCalculatorConstants.MaximumFrequency+"]";
-                return false;
-            } if (_viewModel.ChosenDistance < OpacityCalculatorConstants.MinimumDistance ||
-                  _viewModel.ChosenDistance > OpacityCalculatorConstants.MaximumDistance)
-            {
-                errorMessage = "Distance: "+_viewModel.ChosenDistance+" Outside Range ["+OpacityCalculatorConstants.MinimumDistance+", "+OpacityCalculatorConstants.MaximumDistance+"]";
+                errorMessage += "Frequency: " + _viewModel.ChosenFrequency * 1.0E18 + " Outside Range [" +
+                                OpacityCalculatorConstants.MinimumFrequency + ", " +
+                                OpacityCalculatorConstants.MaximumFrequency + "]";
                 return false;
             }
 
-            if (_viewModel.ChosenIntensity <= OpacityCalculatorConstants.MinimumIntensity || 
-                _viewModel.ChosenIntensity >= OpacityCalculatorConstants.MaximumIntensity)
+            if (_viewModel.ChosenDistance < OpacityCalculatorConstants.MinimumDistance ||
+                _viewModel.ChosenDistance > OpacityCalculatorConstants.MaximumDistance)
             {
-                errorMessage = "Intensity: "+_viewModel.ChosenIntensity+" Outside Range ["+OpacityCalculatorConstants.MinimumIntensity+", "+OpacityCalculatorConstants.MaximumIntensity+"]";
+                errorMessage += "Distance: " + _viewModel.ChosenDistance + " Outside Range [" +
+                                OpacityCalculatorConstants.MinimumDistance + ", " +
+                                OpacityCalculatorConstants.MaximumDistance + "]";
                 return false;
             }
+
+            if (_viewModel.ChosenIntensity < OpacityCalculatorConstants.MinimumIntensity ||
+                _viewModel.ChosenIntensity > OpacityCalculatorConstants.MaximumIntensity)
+            {
+                errorMessage += "Intensity: " + _viewModel.ChosenIntensity + " Outside Range [" +
+                                OpacityCalculatorConstants.MinimumIntensity + ", " +
+                                OpacityCalculatorConstants.MaximumIntensity + "]";
+                return false;
+            }
+
             return true;
         }
-        
+
         private void PlotFrequencyButton_OnClick(object sender, RoutedEventArgs e)
         {
             //Todo G. Call ValidateInputVariables(out var errorMessage) and display error message to errorMessageLabel and return if input variable validation fails
             //Todo     Also use PlotView.Model = newPlotModel() to clear the plot if validation fails
             //Todo     Assign "N/A" to the calculatedIntensityLabel if validation fails
             //Todo     Make sure to clear the error message if there is no error
-                
-            var booly = ValidateInputVariables(out var errorMessage);
-            if (booly == false)
-            {
-                errorMessageLabel.Content = errorMessage;
-            }
-            else
-            {
-                errorMessageLabel.Content = "";
 
-                PlotView.Model = new PlotModel();
+            bool valid = ValidateInputVariables(out var errorMessage);
 
-            }
-            // System.Diagnostics.Debug.WriteLine("booly: " + booly);
-            // Console.WriteLine("booly: "+booly);
-            
+
             var newModel = new PlotModel()
             {
-                Title = $"Intensity through {_viewModel.ChosenDistance}m of {_viewModel.ChosenMaterialType.ToString()} across frequency range"
+                Title =
+                    $"Intensity through {_viewModel.ChosenDistance}m of {_viewModel.ChosenMaterialType.ToString()} across frequency range"
             };
             var xAxis = new LinearAxis
             {
@@ -111,21 +128,34 @@ namespace CompassInterviewTest
             //Todo D. Similarly to how its done in PlotDistanceButton_OnClick, add a function series to the newModel variable
             //Todo     This function series will plot the calculated intensity from the minimum frequency to the maximum frequency, incrementing by the frequency increment constant. All of these are given the OpacityCalculatorConstants.cs
             //Todo     The function will use the input variable as frequency rather than the ChosenFrequency variable
-            
+
             PlotView.Model = newModel;
-            
-            UpdateCalculatedIntensity();
+
+            if (valid)
+            {
+                UpdateCalculatedIntensity();
+            }
+            else
+            {
+                errorMessageLabel.Content = errorMessage;
+                calculatedIntensityLabel.Content = "N/A";
+                PlotView.Model = new PlotModel();
+            }
         }
+
         private void PlotDistanceButton_OnClick(object sender, RoutedEventArgs e)
         {
             //Todo H. (Identical to G) Call ValidateInputVariables(out var errorMessage) and display error message to errorMessageLabel and return if input variable validation fails
             //Todo     Also use PlotView.Model = newPlotModel() to clear the plot if validation fails
             //Todo     Assign "N/A" to the calculatedIntensityLabel if validation fails
             //Todo     Make sure to clear the error message if there is no error
-                
+
+            bool valid = ValidateInputVariables(out var errorMessage);
+          
             var newModel = new PlotModel()
             {
-                Title = $"Intensity of {_viewModel.ChosenFrequency}EHz radiation through {_viewModel.ChosenMaterialType.ToString()} across to {_viewModel.ChosenDistance}m"
+                Title =
+                    $"Intensity of {_viewModel.ChosenFrequency}EHz radiation through {_viewModel.ChosenMaterialType.ToString()} across to {_viewModel.ChosenDistance}m"
             };
             var xAxis = new LinearAxis
             {
@@ -138,18 +168,30 @@ namespace CompassInterviewTest
                 TitleFontSize = 16,
             };
             newModel.Axes.Add(xAxis);
-            
+
             //Plot calculated intensity from the minimum distance to the ChosenDistance variable
-            newModel.Series.Add( 
+            newModel.Series.Add(
                 //Convert Chosen Frequency to exahertz
-                new FunctionSeries(x => OpacityCalculatorConstants.OpacityDistanceFunction(_viewModel.ChosenMaterialType, _viewModel.ChosenIntensity, _viewModel.ChosenFrequency * 1.0E18, x),
-                OpacityCalculatorConstants.MinimumDistance,
-                _viewModel.ChosenDistance,
-                (_viewModel.ChosenDistance - OpacityCalculatorConstants.MinimumDistance) / OpacityCalculatorConstants.DistanceIncrement));
+                new FunctionSeries(
+                    x => OpacityCalculatorConstants.OpacityDistanceFunction(_viewModel.ChosenMaterialType,
+                        _viewModel.ChosenIntensity, _viewModel.ChosenFrequency * 1.0E18, x),
+                    OpacityCalculatorConstants.MinimumDistance,
+                    _viewModel.ChosenDistance,
+                    (_viewModel.ChosenDistance - OpacityCalculatorConstants.MinimumDistance) /
+                    OpacityCalculatorConstants.DistanceIncrement));
 
             PlotView.Model = newModel;
-            
-            UpdateCalculatedIntensity();
+
+            if (valid)
+            {
+                UpdateCalculatedIntensity();
+            }
+            else
+            {
+                errorMessageLabel.Content = errorMessage;
+                calculatedIntensityLabel.Content = "N/A";
+                PlotView.Model = new PlotModel();
+            }
         }
     }
 }
